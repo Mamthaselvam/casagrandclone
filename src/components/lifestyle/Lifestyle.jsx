13a2675e9e8a51eb4987af9a1ed2caf3589da801
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-01-26 20:20:55 +0600

    fix issue

--- src/components/lifestyle/Lifestyle.jsx
+++ src/components/lifestyle/Lifestyle.jsx
@@ -1,101 +1,63 @@
+import React from "react";
 import { Carousel } from "react-bootstrap";
 import { HiArrowLongRight, HiArrowLongLeft } from "react-icons/hi2";
+import "./lifestyle.scss";
 import lifestyle1 from "../../assets/lifestyle1.webp";
 import life2 from "../../assets/life2.jpg";
 import life3 from "../../assets/life3.jpg";
-import "./lifestyle.scss";
 
 const Lifestyle = () => {
   return (
-    <section id="lifeStyle">
-      <Carousel className="position-relative">
+    <div>
+      <Carousel
+        nextIcon={<HiArrowLongRight className="carousel-icon right-icon" />}
+        prevIcon={<HiArrowLongLeft className="carousel-icon left-icon" />}
+        className="custom-carousel"
+      >
         <Carousel.Item>
-          <div className="position-relative">
-            <img className="d-block w-100" src={life2} alt="First slide" />
-            <div className="position-absolute bottom-0 start-0 ms-5 mt-5">
-              <div className=" click h-1/5 w-3/5 bg-white  mt-4">
-                <p className="fs-2">LIFESTYLE AT CASAGRAND</p>
-                <p className="fs-7">
-                  Over 40,000 happy families have found their dream home and
-                  Casagrand properties.
-                </p>
-                {/* <div className="buttons">
-                  <button className="left ">
-                    <span>
-                      <HiArrowLongLeft />
-                    </span>
-                  </button>
-
-                  <button className="leftarrow ">
-                    <span>
-                      <HiArrowLongRight />
-                    </span>
-                  </button>
-                </div> */}
-              </div>
-            </div>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
+          {/* Slide 1 */}
           <div className="position-relative">
             <img
-              className="d-block h-100"
-              src={lifestyle1}
-              alt="Second slide"
+              className="d-block w-100"
+              src={life2}
+              alt="Lifestyle Slide 1"
             />
             <div className="position-absolute bottom-0 start-0 ms-5 mt-5">
-              <div className=" click h-1/5 w-3/5 bg-white  mt-4">
+              <div className="custom-overlay">
                 <p className="fs-2">LIFESTYLE AT CASAGRAND</p>
                 <p className="fs-7">
-                  Over 40,000 happy families have found their dream home and
+                  Over 40,000 happy families have found their dream home at
                   Casagrand properties.
                 </p>
-                {/* <div className="buttons">
-                  <button className="left ">
-                    <span>
-                      <HiArrowLongLeft />
-                    </span>
-                  </button>
-
-                  <button className="leftarrow ">
-                    <span>
-                      <HiArrowLongRight />
-                    </span>
-                  </button>
-                </div> */}
               </div>
             </div>
           </div>
         </Carousel.Item>
+        {/* Add other Carousel Items as needed */}
         <Carousel.Item>
+          {/* Slide 2 */}
           <div className="position-relative">
-            <img className="d-block w-100" src={life3} alt="third slide" />
-            <div className="position-absolute bottom-0 start-0 ms-5 mt-5 border-r-4 shadow-slate-400   ">
-              <div className=" click h-1/5 w-3/5 bg-white  mt-4">
-                <p className="fs-2">LIFESTYLE AT CASAGRAND</p>
-                <p className="fs-7">
-                  Over 40,000 happy families have found their dream home and
-                  Casagrand properties.
-                </p>
-                {/* <div className="buttons">
-                  <button className="left ">
-                    <span>
-                      <HiArrowLongLeft />
-                    </span>
-                  </button>
-
-                  <button className="leftarrow ">
-                    <span>
-                      <HiArrowLongRight />
-                    </span>
-                  </button>
-                </div> */}
-              </div>
-            </div>
+            <img
+              className="d-block w-100"
+              src={lifestyle1}
+              alt="Lifestyle Slide 2"
+            />
+            {/* Similar content structure as Slide 1 */}
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          {/* Slide 3 */}
+          <div className="position-relative">
+            <img
+              className="d-block w-100"
+              src={life3}
+              alt="Lifestyle Slide 3"
+            />
+            {/* Similar content structure as Slide 1 */}
           </div>
         </Carousel.Item>
       </Carousel>
-    </section>
+    </div>
   );
 };
 
