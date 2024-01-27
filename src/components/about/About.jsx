04215921f870babe1289e5245b5778c12d15be38
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-01-27 12:25:06 +0600

    about part added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,97 +1,114 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import { FaHome, FaBuilding,FaRegSmile } from 'react-icons/fa';
-import { MdGroup } from 'react-icons/md';
+import "./about.scss";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import { FaHome, FaBuilding, FaRegSmile } from "react-icons/fa";
+import { MdGroup } from "react-icons/md";
 import { PiBracketsSquareLight } from "react-icons/pi";
-
-import { SlBadge } from 'react-icons/sl';
-import './about.scss';
+import { SlBadge } from "react-icons/sl";
+import aboutImg from "../../assets/aboutImg.jpg";
+import { FaPlay } from "react-icons/fa6";
 
 const About = () => {
   return (
-    <Container className="text1">
-      <h1 className="casagrand-title">ABOUT CASAGRAND</h1>
+    <>
+      <section id="about">
+        <Container>
+          <div className="text-center">
+            <h2>ABOUT CASAGRAND</h2>
+            <h6 className="my-4">SOUTH INDIA'S LEADING REAL ESTATE COMPANY</h6>
+          </div>
 
-     <h2 className="casagrand-subtitle">SOUTH INDIA'S LEADING REAL ESTATE COMPANY</h2>
+          {/* content */}
+          <Row>
+            <Col xl={7} className="mx-auto text-center">
+              <div className="d-flex justify-content-center align-content-center">
+                <div>
+                  <div>
+                    <FaHome />
+                  </div>
+                  <div>DESIGNING AND INNOVATING GREAT HOMES</div>
+                </div>
 
-      <Row>
-        <Col>
-          <p>
-  <span className="casagrand-icon">
-     <FaHome size="1rem" /> 
-  </span>
-  <br />
-  <span className="casagrand-text1">DESIGNING AND INNOVATING</span>
-  <br />
-  <span className="casagrand-text1">GREAT HOMES</span>
-</p>
-        </Col>
-        <Col>
-              <p>
-  <span className="casagrand-icon">
-     <MdGroup size="1rem" /> 
-  </span>
-  <br />
-  <span className="casagrand-text1">BUILDING ASPIRATIONS</span>
-  <br />
-  <span className="casagrand-text1">WITH OUR HOMES</span>
-</p>
-        </Col>
-        <Col>
-        <p>
-  <span className="casagrand-icon">
-     < SlBadge  size="1rem" /> 
-  </span>
-  <br />
-  <span className="casagrand-text1">DELIVERING ON TIME,</span>
-  <br />
-  <span className="casagrand-text1">EVERY TIME</span>
-</p>
-        </Col>
-      </Row>
-    <Row>
-     <Col> <p className="casagrand-paragraph">
-        Casagrand Builder Private Limited is a real estate enterprise committed to building aspirations and delivering value. In the last nineteen years, we have developed over 38 million sqft of prime residential real estate across Chennai, Bengaluru, and Coimbatore. Over 40,000 happy families across 140+ landmark properties stand testimony to our commitment.
+                <div>
+                  <div>
+                    <FaHome />
+                  </div>
+                  <div>DESIGNING AND INNOVATING GREAT HOMES</div>
+                </div>
+                <div>
+                  <div>
+                    <FaHome />
+                  </div>
+                  <div>DESIGNING AND INNOVATING GREAT HOMES</div>
+                </div>
+              </div>
+            </Col>
+          </Row>
 
-In the nineteen years of our journey, we at Casagrand are all set to progress further forward with projects worth over ₹8000 crores in the pipeline.
-      
-      <br/>
-      <br/>
-      
-      In the nineteen years of our journey, we at Casagrand are all set to progress further forward with projects worth over ₹8000 crores in the pipeline.
-      </p>
-      
-      <p className="casagrand-container">
-  <span className="casagrand-icon1">
-    <FaBuilding size="1rem" />
-  </span>
-  <span className="casagrand-text1">140+
-</span>
-  <br />
-  <span className="casagrand-text1">LANDMARKS</span>
-</p>
-<p className="casagrand-container">
-  <span className="casagrand-icon1">
-    <FaRegSmile size="1rem" />
-  </span>
-  <span className="casagrand-text1">40000+</span>
-  <span className="casagrand-text1">HAPPY CUSTOMERS</span>
-</p>
-<p className="casagrand-container">
-  <span className="casagrand-icon1">
-    <PiBracketsSquareLight size="1rem" />
-  </span>
-  <span className="casagrand-text1">38 MILLION+</span>
-  <span className="casagrand-text1">SQ.FEET DEVELOPED</span>
-</p>
-      </Col>
-      <Col>
-  
-     
+          <Row className="mt-5">
+            <Col xl={6}>
+              <div>
+                <p>
+                  Casagrand Builder Private Limited is a real estate enterprise
+                  committed to building aspirations and delivering value. In the
+                  last nineteen years, we have developed over 38 million sqft of
+                  prime residential real estate across Chennai, Bengaluru, and
+                  Coimbatore. Over 40,000 happy families across 140+ landmark
+                  properties stand testimony to our commitment.
+                </p>
 
-      </Col>
-    </Row>
-    </Container>
+                <p>
+                  In the nineteen years of our journey, we at Casagrand are all
+                  set to progress further forward with projects worth over ₹8000
+                  crores in the pipeline.
+                </p>
+
+                <div className="d-flex align-content-center justify-content-evenly">
+                  <div className="d-flex">
+                    <span className="mx-3">
+                      <FaBuilding />
+                    </span>
+                    <div>
+                      <p className="mb-0">140+</p>
+                      <p className="mb-0">LANDMARKS</p>
+                    </div>
+                  </div>
+
+                  <div className="d-flex">
+                    <span className="mx-3">
+                      <FaBuilding />
+                    </span>
+                    <div>
+                      <p className="mb-0">140+</p>
+                      <p className="mb-0">LANDMARKS</p>
+                    </div>
+                  </div>
+
+                  <div className="d-flex">
+                    <span className="mx-3">
+                      <FaBuilding />
+                    </span>
+                    <div>
+                      <p className="mb-0">140+</p>
+                      <p className="mb-0">LANDMARKS</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Col>
+            <Col xl={6}>
+              <div className="aboutRight">
+                <Image src={aboutImg} alt="about" />
+                <div className="ytPlay">
+                  <a href="/" className="text-black">
+                    <FaPlay style={{ fontSize: "1.5rem" }} />
+                  </a>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   );
 };
 
