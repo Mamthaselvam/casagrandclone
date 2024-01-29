19caada597d2305aca63caa8420b6c4bd1b4fa7f
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-01-29 16:08:32 +0600

    fixed some bug

--- src/components/footer/RightWidget.jsx
+++ src/components/footer/RightWidget.jsx
@@ -1,8 +1,10 @@
 import { Drawer } from "antd";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import phone from "../../assets/phone.webp";
 import whatsapp from "../../assets/whatsapp.webp";
 import { FaRegWindowClose } from "react-icons/fa";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const RightWidget = () => {
   const [open, setOpen] = useState(false);
@@ -14,6 +16,14 @@ const RightWidget = () => {
   const onClose = () => {
     setOpen(false);
   };
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1200,
+      easing: "ease",
+      offset: 200,
+    });
+  }, []);
 
   return (
     <>
@@ -36,15 +46,15 @@ const RightWidget = () => {
         getContainer={false}
         className="drawer"
       >
-        <div className="info">
-          <p>Chennai</p>
+        <div className="info" data-aos="fade-left">
+          <p className="fw-bold mb-0">Chennai</p>
           <p> 99629 44444</p>
-          <p>Coimbatore</p>
+          <p className="fw-bold mb-0">Coimbatore</p>
+          <p className="mb-0"> 99629 44444</p>
+          <p className="mb-0">Bengaluru</p>
           <p> 99629 44444</p>
-          <p>Bengaluru</p>
-          <p> 99629 44444</p>
-          <p>Hyderabad</p>
-          <p> 99629 44444</p>
+          <p className="fw-bold mb-0">Hyderabad</p>
+          <p className="mb-0"> 99629 44444</p>
         </div>
         <div onClick={onClose} className="close">
           <FaRegWindowClose />
