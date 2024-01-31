d6de2fb5ad72119e7600970102e256d9db312a2a
Mamthaselvam <mamthapselvam@gmail.com>
2024-01-31 18:48:02 +0530

    added values in  aboutus

--- src/pages/aboutUs/AboutUs.jsx
+++ src/pages/aboutUs/AboutUs.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Container,Row,Col} from 'react-bootstrap';
+import './aboutUs.scss'
 
 const AboutUs = () => {
   return (
@@ -9,7 +11,25 @@ const AboutUs = () => {
        </div>
        {/* values part */}
        <div>
-           value
+           <h1>Our Core Values</h1>
+           <Container className='mt-5'>
+           <Row xs={1} md={2}>
+              <Col className='mt-5 '>
+              <h2>Our Mission</h2>
+              <p>With over $2 Billion in sales, Our agency is the industry’s top luxury producer with over 27 years of experience in marketing Seattles’s most prestigious waterfront properties. </p>
+              </Col>
+              <Col  className='mt-5 '><h2>Our Vision</h2>
+              <p>With over $2 Billion in sales, Our agency is the industry’s top luxury producer with over 27 years of experience in marketing Seattles’s most prestigious waterfront properties. </p></Col>
+              <Col className='mt-5'><h2>Our Values</h2>
+              <p>With over $2 Billion in sales, Our agency is the industry’s top luxury producer with over 27 years of experience in marketing Seattles’s most prestigious waterfront properties. </p></Col>
+              <Col className='mt-5'><h2>Our Resources</h2>
+              <p>With over $2 Billion in sales, Our agency is the industry’s top luxury producer with over 27 years of experience in marketing Seattles’s most prestigious waterfront properties. </p></Col>
+            </Row>
+           </Container>
+       </div>
+       {/* picture part */}
+       <div>
+        picture
        </div>
     </div>
   )
